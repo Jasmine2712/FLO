8f951a504f8ddbcbef791b471563d111b85f9e8e
Jasmine2712 <jazzahra341@gmail.com>
2021-11-20 23:20:38 +0700

    jasmine

---
+++ app/src/main/java/com/example/flo/Success.java
@@ -0,0 +1,25 @@
+package com.example.flo;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+
+public class Success extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_success);
+    }
+
+    public void camera(View view) {
+        Intent intent = new Intent(this, KondisiAirActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    public void water(View view) {
+    }
+}
